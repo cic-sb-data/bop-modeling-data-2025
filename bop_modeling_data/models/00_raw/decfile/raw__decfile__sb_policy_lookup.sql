5465f165e77c0eb720f0d9ff76c0b2b4cd415575
Andy Weaver <andrewayersweaver@gmail.com>
2025-04-28 09:19:50 -0400

    updated docs for policy lookup

--- bop_modeling_data/models/00_raw/decfile/raw__decfile__sb_policy_lookup.sql
+++ bop_modeling_data/models/00_raw/decfile/raw__decfile__sb_policy_lookup.sql
@@ -2,7 +2,14 @@ with
 
 raw as (
     select
-        *
+        sb_policy_key,
+        policy_chain_id,
+        lob,
+        company_numb,
+        policy_sym,
+        policy_numb,
+        policy_module,
+        policy_eff_date
 
     from read_csv_auto('{{ var("raw_csv_loc") }}/decfile.policy_lookup.csv')
 )
