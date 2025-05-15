bbab9544819bc3cc328ad546371a9e98d0ff115f
Andy Weaver <andrewayersweaver@gmail.com>
2025-05-15 09:31:09 -0400

    back to the server with better logging

--- bop_modeling_data/macros/bil_table_lookup.sql
+++ bop_modeling_data/macros/bil_table_lookup.sql
@@ -2,7 +2,10 @@
     {%- set relation = _get_xcd_bil_relation(xcd_bil_table) -%}
     {%- set key_name = _get_xcd_bil_key_name(xcd_bil_table) -%}
 
-{{ log('xcd-bil-log-1') }}
+    {{ log('xcd_bil_table: ' ~ xcd_bil_table) }}
+    {{ log('primary_keys:\n' ~ primary_keys) }}
+    {{ log('relation:\n' ~ relation) }}
+    {{ log('key_name:\n' ~ key_name) }}
     with
 
     raw as (
@@ -11,7 +14,6 @@
         order by {{ _get_new_cols(primary_keys) }}
     ),
 
-{{ log('xcd-bil-log-2') }}
     add_key as (
         select 
             row_number() over() as {{ key_name }},
@@ -19,46 +21,38 @@
         from raw
     ),
 
-{{ log('xcd-bil-log-3') }}
     sort_table as (
         select *
         from add_key
         order by {{ key_name }}
     )
 
-{{ log('xcd-bil-log-4') }}
     select *
     from sort_table
 
 {%- endmacro -%}
 
 {%- macro _select_and_rename_cols(primary_keys) -%}
-{{ log('xcd-bil-log-5') }}
     {% for old, new in primary_keys %}
+        {{ log('old:\n' ~ old) }}
+        {{ log('new:\n' ~ new) }}
         {{ old }} as {{ new }}{%- if not loop.last -%},{%- endif -%}
     {%- endfor %}
-{{ log('xcd-bil-log-6') }}
 {%- endmacro -%}
 
 {%- macro _get_new_cols(primary_keys) -%}
-{{ log('xcd-bil-log-7') }}
     {% for old, new in primary_keys %}
         {{ new }}{%- if not loop.last -%},{%- endif -%}
-    {%- endfor %}
 {{ log('xcd-bil-log-8') }}
 {%- endmacro -%}
 
 {%- macro _get_xcd_bil_relation(xcd_bil_table) -%}
-{{ log('xcd-bil-log-9') }}
     {%- set relation='raw__screngn__xcd_bil_' ~ xcd_bil_table -%}
     {{ relation }}
-{{ log('xcd-bil-log-10') }}
 {%- endmacro -%}
 
 {%- macro _get_xcd_bil_key_name(xcd_bil_table) -%}
-{{ log('xcd-bil-log-11') }}
     {%- set key_name='bil_' ~ xcd_bil_table ~ '_key' -%}
     {{ key_name }}
-{{ log('xcd-bil-log-12') }}
 {%- endmacro -%}
 
