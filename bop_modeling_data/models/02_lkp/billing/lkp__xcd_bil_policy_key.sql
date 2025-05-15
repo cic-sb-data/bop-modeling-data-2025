c22f75b1fa4ee599520e460d36fed7f8508f5ac7
Andy Weaver <andrewayersweaver@gmail.com>
2025-05-15 09:12:15 -0400

    back to the server

--- bop_modeling_data/models/02_lkp/billing/lkp__xcd_bil_policy_key.sql
+++ bop_modeling_data/models/02_lkp/billing/lkp__xcd_bil_policy_key.sql
@@ -7,7 +7,7 @@
 with 
 
 lkp as ({{ bil_table_lookup(xcd_bil_table, primary_keys) }}),
-add_acct_key as ({{ add_bil_account_key('lkp') }}),
+add_acct_key as ({{ add_bil_acct_key('lkp') }}),
 
 recoded as (
     select
