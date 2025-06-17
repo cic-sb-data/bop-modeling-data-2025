3400c1f7f68b82fef5e63b4c20570c2bcc287314
Andy Weaver <andrewayersweaver@gmail.com>
2025-06-17 12:52:33 -0400

    hopefully removed some newlines??

--- bop_modeling_data/macros/lookups/_surrogate_name.sql
+++ bop_modeling_data/macros/lookups/_surrogate_name.sql
@@ -4,9 +4,24 @@
    If no surrogate name is provided, it defaults to a combination of the first column name and '_id'.
    If multiple columns are provided, it defaults to 'surrogate_id'. #}
 {% macro _lookups__surrogate_name(id_col_name, column_names ) -%}
-    {% if column_names | length == 1 %}
-        {{ column_names[0] ~ '_id' }}
-    {% else %}
-        {{ id_col_name }}
-    {% endif %}
-{%- endmacro %}
+    {%- if column_names | length == 1 -%}
+        {%- set surrogate_name = column_names[0] ~ '_id' -%}
+    {%- else -%}
+        {%- set surrogate_name = id_col_name -%}
+    {%- endif -%}
+
+    {%- set clean_surrogate_name = __clean_text(surrogate_name) -%}
+    {%- set surrogate_name = 'lkp__' ~ clean_surrogate_name -%}
+
+    {%- if surrogate_name | length > 63 -%}
+        {%- set surrogate_name = surrogate_name[:63] -%}
+    {%- endif -%}
+
+    {{ surrogate_name }}
+
+{%- endmacro -%}
+
+{%- macro __clean_text(text) -%}
+    {%- set cleaned_text = text | trim | lower | replace(' ', '_') -%}
+    {{ cleaned_text }}
+{%- endmacro -%}
