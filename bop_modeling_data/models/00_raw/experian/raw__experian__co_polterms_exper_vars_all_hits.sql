c4e3884e1566ec1db3646fc90d35ad3de0a4d032
Andy Weaver <andrewayersweaver@gmail.com>
2025-06-18 15:39:45 -0400

    increased sample size

--- bop_modeling_data/models/00_raw/experian/raw__experian__co_polterms_exper_vars_all_hits.sql
+++ bop_modeling_data/models/00_raw/experian/raw__experian__co_polterms_exper_vars_all_hits.sql
@@ -3,7 +3,7 @@ with
 
 raw as (
     select *
-    from read_csv_auto('{{ var("raw_csv_loc") }}/experian.csv')
+    from read_csv_auto('{{ var("raw_csv_loc") }}/experian.csv', sample_size=1000000)
 )
 
 select *
