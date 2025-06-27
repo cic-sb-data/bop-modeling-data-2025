58951a0e4241fd299cb29671ff6c41b46e8a97c2
Andy Weaver <andrewayersweaver@gmail.com>
2025-06-27 13:51:45 -0400

    I might be dumb

--- bop_modeling_data/tests/generic/assert_min_max_value_.sql
+++ bop_modeling_data/tests/generic/assert_min_max_value_.sql
@@ -1,24 +1,32 @@
+-- filepath: /home/aweaver/work/bop-modeling-data-2025/bop_modeling_data/tests/generic/assert_min_max_value_.sql
 {% test assert_max_value_lt(model, column_name, max_value) %}
+    {% set col = column_name if column_name is not none else kwargs.get('column_name') %}
+    {% set maxv = max_value if max_value is not none else kwargs.get('max_value') %}
     select *
     from {{ model }}
-    where {{ column_name }} >= {{ max_value }}
+    where {{ col }} >= {{ maxv }}
 {% endtest %}
 
 {% test assert_max_value_le(model, column_name, max_value) %}
+    {% set col = column_name if column_name is not none else kwargs.get('column_name') %}
+    {% set maxv = max_value if max_value is not none else kwargs.get('max_value') %}
     select *
     from {{ model }}
-    where {{ column_name }} > {{ max_value }}
+    where {{ col }} > {{ maxv }}
 {% endtest %}
 
 {% test assert_min_value_gt(model, column_name, min_value) %}
+    {% set col = column_name if column_name is not none else kwargs.get('column_name') %}
+    {% set minv = min_value if min_value is not none else kwargs.get('min_value') %}
     select *
     from {{ model }}
-    where {{ column_name }} <= {{ min_value }}
+    where {{ col }} <= {{ minv }}
 {% endtest %}
 
-
 {% test assert_min_value_ge(model, column_name, min_value) %}
+    {% set col = column_name if column_name is not none else kwargs.get('column_name') %}
+    {% set minv = min_value if min_value is not none else kwargs.get('min_value') %}
     select *
     from {{ model }}
-    where {{ column_name }} > {{ min_value }}
+    where {{ col }} < {{ minv }}
 {% endtest %}
