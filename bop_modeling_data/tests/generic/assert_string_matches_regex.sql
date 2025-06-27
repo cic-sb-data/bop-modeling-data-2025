253def52497d17d6140d778f463592d50b3ecfbf
Andy Weaver <andrewayersweaver@gmail.com>
2025-06-27 14:30:25 -0400

    maybe maybe maybe

--- bop_modeling_data/tests/generic/assert_string_matches_regex.sql
+++ bop_modeling_data/tests/generic/assert_string_matches_regex.sql
@@ -1,7 +1,7 @@
-{%- test assert_string_matches_regex(model, column_name, regex) -%}
+{% test assert_string_matches_regex(model, column_name, regex) %}
     {% set col = column_name if column_name is not none else kwargs.get('column_name') %}
     {% set rx = regex if regex is not none else kwargs.get('regex') %}
     select *
     from {{ model }}
     where not {{ col }} ~ {{ rx }}
-{%- endtest -%}
+{% endtest %}
