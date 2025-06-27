0eb96d9810fd2af42b99dbf52766e45c7ca885e0
Andy Weaver <andrewayersweaver@gmail.com>
2025-06-27 13:24:54 -0400

    hopefully fixed generic tests

--- bop_modeling_data/tests/generic/assert_min_max_value_.sql
+++ bop_modeling_data/tests/generic/assert_min_max_value_.sql
@@ -1,49 +1,24 @@
 {% test assert_max_value_lt(model, column_name, max_value) %}
-
-    {% set column_name = kwargs.get('column_name') %}
-    {% set max_value = kwargs.get('max_value') %}
-
-    select
-        *
+    select *
     from {{ model }}
-    where {{ column_name }} > {{ max_value }}
-    
+    where {{ column_name }} >= {{ max_value }}
 {% endtest %}
 
 {% test assert_max_value_le(model, column_name, max_value) %}
-
-    {% set column_name = kwargs.get('column_name') %}
-    {% set max_value = kwargs.get('max_value') %}
-
-    select
-        *
+    select *
     from {{ model }}
     where {{ column_name }} > {{ max_value }}
-
 {% endtest %}
 
-
 {% test assert_min_value_gt(model, column_name, min_value) %}
-
-    {% set column_name = kwargs.get('column_name') %}
-    {% set min_value = kwargs.get('min_value') %}
-
-    select
-        *
+    select *
     from {{ model }}
-    where {{ column_name }} < {{ min_value }}
-
+    where {{ column_name }} <= {{ min_value }}
 {% endtest %}
 
 
 {% test assert_min_value_ge(model, column_name, min_value) %}
-
-    {% set column_name = kwargs.get('column_name') %}
-    {% set min_value = kwargs.get('min_value') %}
-
-    select
-        *
+    select *
     from {{ model }}
-    where {{ column_name }} >= {{ min_value }}
-
+    where {{ column_name }} > {{ min_value }}
 {% endtest %}
--- bop_modeling_data/tests/generic/assert_string_matches_regex.sql
+++ bop_modeling_data/tests/generic/assert_string_matches_regex.sql
@@ -1,9 +1,5 @@
 {%- test assert_string_matches_regex(model, column_name, regex) -%}
-    {% set column_name = kwargs.get('column_name') %}
-    {% set regex = kwargs.get('regex') %}
-
-    select
-        *
+    select *
     from {{ model }}
     where not {{ column_name }} ~ {{ regex }}
 {%- endtest -%}
