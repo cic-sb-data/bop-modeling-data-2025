9df47eb9fb61788a2f473c1796630dc730c6c7b3
Andy Weaver <andrewayersweaver@gmail.com>
2025-06-30 10:14:23 -0400

    quotes around interval?

--- bop_modeling_data/macros/date_add.sql
+++ bop_modeling_data/macros/date_add.sql
@@ -3,10 +3,10 @@
     {%- if adapter in ['snowflake', 'redshift', 'postgres'] -%}
         dateadd({{ datepart }}, {{ interval }}, {{ date_expr }})
     {%- elif adapter == 'bigquery' -%}
-        datetime_add({{ date_expr }}, INTERVAL {{ interval }} {{ datepart | upper }})
+        datetime_add({{ date_expr }}, INTERVAL '{{ interval }} {{ datepart | upper }}')
     {%- elif adapter == 'duckdb' -%}
-        date_add({{ date_expr }}, interval {{ interval }} {{ datepart | upper }})
+        date_add({{ date_expr }}, interval '{{ interval }} {{ datepart | upper }}')
     {%- else -%}
-        date_add({{ date_expr }}, interval {{ interval }} {{ datepart | upper }})
+        date_add({{ date_expr }}, interval '{{ interval }} {{ datepart | upper }}')
     {%- endif -%}
 {% endmacro %}
