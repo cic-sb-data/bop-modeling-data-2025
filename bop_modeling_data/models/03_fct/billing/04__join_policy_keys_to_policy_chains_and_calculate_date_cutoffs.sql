cbf418415e00cca9a6c54994ef797a17069887f3
Andy Weaver <andrewayersweaver@gmail.com>
2025-06-30 09:15:30 -0400

    removed column that doesn't exist and use the cte names instead of the aliases

--- bop_modeling_data/models/03_fct/billing/04__join_policy_keys_to_policy_chains_and_calculate_date_cutoffs.sql
+++ bop_modeling_data/models/03_fct/billing/04__join_policy_keys_to_policy_chains_and_calculate_date_cutoffs.sql
@@ -19,56 +19,55 @@ chains as (
 
 joined as (
     select
-        d.sb_aiv_key,
-        d.sb_policy_key,
-        i.company_numb,
-        i.policy_sym,
-        i.policy_numb,
-        i.policy_module,
-        i.policy_eff_date,
-        i.image_eff_date,
-        i.image_exp_date,
-        c.policy_chain_id,
+        deduped.sb_aiv_key,
+        deduped.sb_policy_key,
+        images.company_numb,
+        images.policy_sym,
+        images.policy_numb,
+        images.policy_module,
+        images.policy_eff_date,
+        images.image_eff_date,
+        chains.policy_chain_id,
 
         -- Claims evaluation and cutoff dates (SAS intnx logic)
-        dateadd(month, -4, i.image_eff_date) as clm_eval_date,
+        dateadd(month, -4, images.image_eff_date) as clm_eval_date,
 
-        dateadd(month, -8,  dateadd(month, -4, i.image_eff_date))  as clm_prev_1yr_start,
-        dateadd(month, -20, dateadd(month, -4, i.image_eff_date))  as clm_prev_2yr_start,
-        dateadd(month, -32, dateadd(month, -4, i.image_eff_date))  as clm_prev_3yr_start,
-        dateadd(month, -44, dateadd(month, -4, i.image_eff_date))  as clm_prev_4yr_start,
-        dateadd(month, -56, dateadd(month, -4, i.image_eff_date))  as clm_prev_5yr_start,
+        dateadd(month, -8,  dateadd(month, -4, images.image_eff_date))  as clm_prev_1yr_start,
+        dateadd(month, -20, dateadd(month, -4, images.image_eff_date))  as clm_prev_2yr_start,
+        dateadd(month, -32, dateadd(month, -4, images.image_eff_date))  as clm_prev_3yr_start,
+        dateadd(month, -44, dateadd(month, -4, images.image_eff_date))  as clm_prev_4yr_start,
+        dateadd(month, -56, dateadd(month, -4, images.image_eff_date))  as clm_prev_5yr_start,
 
-        dateadd(month, -4, i.image_eff_date)                      as clm_prev_1yr_end,
-        dateadd(month, -8,  dateadd(month, -4, i.image_eff_date)) as clm_prev_2yr_end,
-        dateadd(month, -20, dateadd(month, -4, i.image_eff_date)) as clm_prev_3yr_end,
-        dateadd(month, -32, dateadd(month, -4, i.image_eff_date)) as clm_prev_4yr_end,
-        dateadd(month, -44, dateadd(month, -4, i.image_eff_date)) as clm_prev_5yr_end,
+        dateadd(month, -4, images.image_eff_date)                      as clm_prev_1yr_end,
+        dateadd(month, -8,  dateadd(month, -4, images.image_eff_date)) as clm_prev_2yr_end,
+        dateadd(month, -20, dateadd(month, -4, images.image_eff_date)) as clm_prev_3yr_end,
+        dateadd(month, -32, dateadd(month, -4, images.image_eff_date)) as clm_prev_4yr_end,
+        dateadd(month, -44, dateadd(month, -4, images.image_eff_date)) as clm_prev_5yr_end,
 
         -- Billing evaluation and cutoff dates (SAS intnx logic)
-        dateadd(month, -4, i.image_eff_date) as bil_eval_date,
+        dateadd(month, -4, images.image_eff_date) as bil_eval_date,
 
-        dateadd(year, -1, i.image_eff_date) as bil_prev_1yr_start,
-        dateadd(year, -2, i.image_eff_date) as bil_prev_2yr_start,
-        dateadd(year, -3, i.image_eff_date) as bil_prev_3yr_start,
-        dateadd(year, -4, i.image_eff_date) as bil_prev_4yr_start,
-        dateadd(year, -5, i.image_eff_date) as bil_prev_5yr_start,
+        dateadd(year, -1, images.image_eff_date) as bil_prev_1yr_start,
+        dateadd(year, -2, images.image_eff_date) as bil_prev_2yr_start,
+        dateadd(year, -3, images.image_eff_date) as bil_prev_3yr_start,
+        dateadd(year, -4, images.image_eff_date) as bil_prev_4yr_start,
+        dateadd(year, -5, images.image_eff_date) as bil_prev_5yr_start,
 
-        dateadd(month, -4, i.image_eff_date) as bil_prev_1yr_end,
-        dateadd(year, -1, i.image_eff_date)  as bil_prev_2yr_end,
-        dateadd(year, -2, i.image_eff_date)  as bil_prev_3yr_end,
-        dateadd(year, -3, i.image_eff_date)  as bil_prev_4yr_end,
-        dateadd(year, -4, i.image_eff_date)  as bil_prev_5yr_end
+        dateadd(month, -4, images.image_eff_date) as bil_prev_1yr_end,
+        dateadd(year, -1, images.image_eff_date)  as bil_prev_2yr_end,
+        dateadd(year, -2, images.image_eff_date)  as bil_prev_3yr_end,
+        dateadd(year, -3, images.image_eff_date)  as bil_prev_4yr_end,
+        dateadd(year, -4, images.image_eff_date)  as bil_prev_5yr_end
 
-    from deduped d
-    left join images i
-        on d.sb_aiv_key = i.sb_aiv_key
-    left join chains c
-        on i.company_numb = c.company_numb
-        and i.policy_sym = c.policy_sym
-        and i.policy_numb = c.policy_numb
-        and i.policy_module = c.policy_module
-        and i.policy_eff_date = c.policy_eff_date
+    from deduped
+    left join images
+        on deduped.sb_aiv_key = images.sb_aiv_key
+    left join chains
+        on images.company_numb = chains.company_numb
+        and images.policy_sym = chains.policy_sym
+        and images.policy_numb = chains.policy_numb
+        and images.policy_module = chains.policy_module
+        and images.policy_eff_date = chains.policy_eff_date
 )
 
 select * from joined
