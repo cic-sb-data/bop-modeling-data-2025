320fe898a17423b4ed44ac9eeb9a159f481e89af
Andy Weaver <andrewayersweaver@gmail.com>
2025-07-03 16:47:08 -0400

    closer tho not done

--- bop_modeling_data/models/01_stg/screngn/stg__screngn__xcd_bil_act_summary.sql
+++ bop_modeling_data/models/01_stg/screngn/stg__screngn__xcd_bil_act_summary.sql
@@ -10,24 +10,35 @@ act_summary_key as (select * from {{ ref('lkp__bil_act_summary_key') }}),
 
 recode_and_renamed as (
     select
-        BIL_ACCT_ID as bil_acct_id,
+        BIL_ACCOUNT_ID as bil_acct_id,
         POL_SYMBOL_CD as policy_sym,
         try_cast(POL_NBR as uinteger) as policy_numb,
         BIL_ACY_DES_CD as bil_act_desc_code,
         BIL_DES_REA_TYP as bil_act_reason_type_code,
         {{ recode__sas_date_format('BIL_ACY_DES1_DT') }} as bil_act_desc1_date,
         {{ recode__sas_date_format('BIL_ACY_DES2_DT') }} as bil_act_desc2_date,
+        {{ recode__sas_date_format('BIL_ACY_DT') }} as bil_act_date,
         try_cast(BIL_ACY_AMT as double) as bil_act_amt,
         USER_ID as user_id,
         split(BIL_ACY_TS, ':')[1] as bil_act_time,
         BAS_ADD_DATA_TXT as bil_act_summary_addl_data,
 
-        *
+        * exclude (
+            bil_account_id,
+            bil_acy_des1_dt,
+            bil_acy_des2_dt,
+            bil_acy_dt,
+            pol_symbol_cd,
+            pol_nbr,
+            bil_account_id_hash
+        )
 
     from raw
 ),
 
-add_acct_key as ({{ add_bil_acct_key('recode_and_renamed') }}),
+add_acct_key as (
+    select
+        
 
 joined as (
     select 
@@ -55,4 +66,4 @@ drop_join_cols as (
 )
 
 select *
-from add_acct_key
+from recode_and_renamed
